4c9c8c5dbf4bdadc59f79f7cf206cc289b2a2846
Sunnapu-Rajkumar <117339623+Sunnapu-Rajkumar@users.noreply.github.com>
2025-08-12 17:04:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -61,5 +61,5 @@ def suggest():
         return jsonify({"suggestion": dummy})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port, debug=False)
